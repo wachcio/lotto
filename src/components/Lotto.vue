f6fb73203a5bc69bccabc05babcd71271f74c1a2
wachcio <wachcio@poczta.fm>
2021-05-22 19:24:40 +0200

    Add form

--- src/components/Lotto.vue
+++ src/components/Lotto.vue
@@ -1,42 +1,57 @@
 <template>
-<div>
-    <Games>
-        <Result>
-</div>
+  <div>
+    <div>
+      <form>
+        <label for="game">Wybierz grę z której chcesz poznać wyniki:</label>
+        <select id="game" name="game" v-model="gameSelect">
+          <option v-for="game in games" :key="game.id">{{ game }}</option>
+        </select>
+        <label for="gamesNumber">Wybierz liczbę ostatnich losowań (10-1000):</label>
+        <input
+          type="number"
+          id="gamesNumber"
+          name="gamesNumber"
+          min="10"
+          max="1000"
+          v-model="gamesNumber"
+        />
+      </form>
+    </div>
+    <Result />
+  </div>
 </template>
 
 <script>
-import Games from './Games.vue';
-import Result from './Result.vue';
+import Result from "./Result.vue";
 
 export default {
-  name: 'Lotto',
-  props: {
-
-  },
+  name: "Lotto",
+  props: {},
   data() {
     return {
-
+      games: [
+        "Lotto",
+        "Eurojackpot",
+        "Keno",
+        "Szybkie600",
+        "MultiMulti",
+        "EkstraPensja",
+        "Mini Lotto",
+        "Kaskada",
+        "SuperSzansa"
+      ],
+      gameSelect: "Lotto",
+      gamesNumber: 10
     };
   },
   components: {
-    Games, Result,
+    Result
   },
-  methods: {
-
-  },
-  computed: {
-
-  },
-  created() {
-
-  },
-  watch: {
-
-  },
+  methods: {},
+  computed: {},
+  created() {},
+  watch: {}
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/Result.vue
@@ -0,0 +1,18 @@
+<template><div></div></template>
+
+<script>
+export default {
+  name: "Result",
+  props: {},
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  computed: {},
+  created() {},
+  watch: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
